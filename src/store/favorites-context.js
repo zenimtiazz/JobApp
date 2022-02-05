b6ceb815122b2dd4cc8fc41791ede5d4805a9d88
Zain Imtiaz <90681723+zenimtiazz@users.noreply.github.com>
2022-02-05 14:00:16 +0100

    adding functionality of favorites

---
+++ src/store/favorites-context.js
@@ -0,0 +1,31 @@
+import { createContext,useState } from "react";
+import Favorites from "../pages/Favorites";
+
+const FavoritesContext = createContext({
+    favorites :[],
+    totalFavorites :0
+});
+function FavoritesContextProvider(props){
+    const[userFavorites,setUserFavorites]=useState([]);
+    function addFavoriteHandler(favoriteMeetup){
+ setUserFavorites((prevUserFavorites)=>{
+     return prevUserFavorites.concat(favoriteMeetup);
+ });
+    }
+    function removeFavoriteHandler(meetupId){
+        setUserFavorites((prevUserFavorites)=>{
+return prevUserFavorites.filter(meetup =>meetup.id !== meetupId)
+    });
+}
+    function itemFavoriteHandler(meetupId){
+return userFavorites.some(meetup=>meetup.id===meetupId);
+    }
+ const context ={
+     favorites : userFavorites,
+     totalFavorites:userFavorites.length,
+ };
+
+    return <FavoritesContext.Provider value={context}>
+    {props.chimdren}
+    </FavoritesContext.Provider>
+}
