aa9efe8f37eb994d4a95182189e3de93eede2948
Zain Imtiaz <90681723+zenimtiazz@users.noreply.github.com>
2022-02-05 11:50:26 +0100

    injected the components using props.children

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,6 +1,8 @@
+import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 const MeetupItem = (props) => {
   return <li className={classes.item} >
+    <Card>
     <div className={classes.image}>
     <img src= {props.image} alt={props.title} />
 </div>
@@ -12,8 +14,9 @@ const MeetupItem = (props) => {
 <div className= {classes.actions}>
     <button>To favorites</button>
 </div>
-
+</Card>
   </li>;
+  
 };
 
 export default MeetupItem;
