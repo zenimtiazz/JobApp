92e6f63fd8d17fac0d37a386bd816c73d420b883
Zain Imtiaz <90681723+zenimtiazz@users.noreply.github.com>
2022-02-05 13:01:48 +0100

    use firebase to store data of form

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import Card from "../ui/Card";
 import classes from './NewMeetupForm.module.css';
 
-const NewMeetupForm = () => {
+const NewMeetupForm = (props) => {
    const titleInputRef= useRef();
    const imageInputRef= useRef();
    const addressInputRef= useRef();
@@ -11,7 +11,17 @@ const NewMeetupForm = () => {
     function submitHandler(event){
      event.preventDefault();
      const enteredTitle= titleInputRef.current.value;
+     const enteredImage= imageInputRef.current.value;
+     const enteredAddress= addressInputRef.current.value;
+     const enteredDescription= descriptionInputRef.current.value;
 
+     const meetupData={
+         title:enteredTitle,
+         image: enteredImage,
+         address: enteredAddress,
+         description:enteredDescription,
+     };
+ props.onAddMeetup(meetupData);
     }
   return <Card>
 <form className = {classes.form} onSubmit={submitHandler}>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,9 +1,23 @@
+import { useHistory } from "react-router";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 const NewMeetup = () => {
+  const history = useHistory();
+  function addMeetupHandler(meetupData){
+// to send http request in firebase meetups.json that the format whic firebase hase
+fetch('https://react-meetups-7d806-default-rtdb.firebaseio.com/meetups.json',
+{
+  method:'POST',
+  body: JSON.stringify(meetupData),
+  headers:{
+    'Content-Type':'applications/json'
+  }
+}
+);
+  }
   return (<section>
      <h1>  Add new meetup </h1>
-     <NewMeetupForm />
+     <NewMeetupForm onAddMeetup={addMeetupHandler}/>
   </section>)
 };
 
