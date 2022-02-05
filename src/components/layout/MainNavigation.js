eb3d8304d16e7df1cab1b97c782e4a8f3c1a760a
Zain Imtiaz <90681723+zenimtiazz@users.noreply.github.com>
2022-02-05 14:54:56 +0100

    Styling need to b added

--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,23 +1,35 @@
-import { Link } from "react-router-dom";
-import classes from'./MainNavigation.module.css';
-const MainNavigation = () => {
-  return <header className={classes.header}>
-      <div className= {classes.logo}>React Meetup</div>
-      <nav>
-          <ul>
-              <li>
-                  <Link to='/'>All Meetups</Link>
-              </li>
-              <li>
-                  <Link to='/new-meetup'>Add New Meetups</Link>
-              </li>
-              <li>
-                  <Link to='/favorites'>My Favorites</Link>
-              </li>
+import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 
-          </ul>
+import classes from './MainNavigation.module.css';
+import FavoritesContext from '../../store/favorites-context';
+
+function MainNavigation() {
+  const favoritesCtx = useContext(FavoritesContext);
+
+  return (
+    <header className={classes.header}>
+      <div className={classes.logo}>React Meetups</div>
+      <nav>
+        <ul>
+          <li>
+            <Link to='/'>All Meetups</Link>
+          </li>
+          <li>
+            <Link to='/new-meetup'>Add New Meetup</Link>
+          </li>
+          <li>
+            <Link to='/favorites'>
+              My Favorites
+              <span className={classes.badge}>
+                {favoritesCtx.totalFavorites}
+              </span>
+            </Link>
+          </li>
+        </ul>
       </nav>
-  </header>;
-};
+    </header>
+  );
+}
 
 export default MainNavigation;
